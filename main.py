83afefdb5af561ec326b061b88c5ea12df153b30
tageorge0 <andy3766981@gmail.com>
2025-06-03 14:11:24 +0800

    fix main.py

--- main.py
+++ main.py
@@ -97,3 +97,6 @@ def handle_message(event):
         line_bot_api.push_message(user_id, TextSendMessage(text=summary))
 
     Thread(target=background_analysis).start()
+
+if __name__ == '__main__':
+	    app.run(debug=True, port=os.getenv("PORT", default=5000))

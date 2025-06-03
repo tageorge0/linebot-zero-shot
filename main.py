34eff7d3374def34db604a9422b9fbbd37586457
tageorge0 <andy3766981@gmail.com>
2025-06-03 22:51:41 +0800

    fix

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def callback():
     print("收到 LINE Webhook 請求")
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
     try:
         handler.handle(body, signature)
         print("handler 處理完成")
